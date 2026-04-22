fd3626260ddc4d25b379d634ee4c24797c42e01a
Raj Nandani Maurya <147945705+Nandani-Maurya@users.noreply.github.com>
2026-04-22 12:16:42 +0530

    tender category clear

--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -3,18 +3,13 @@ const router = express.Router();
 const CategoryController = require('../controllers/category.controller');
 const { protect } = require('../middlewares/auth.middleware');
 
-
 router.use(protect);
-
 
 router.get('/tender', CategoryController.getTenderCategories);
 
-
 router.post('/tender', CategoryController.createTenderCategory);
 
-
 router.put('/tender/:id', CategoryController.updateTenderCategory);
-
 
 router.delete('/tender/:id', CategoryController.deleteTenderCategory);
 
